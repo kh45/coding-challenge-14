fb94cb00f882a98146fc4af21dc7a0fe8761b64b
KhaledDev <khaledhassan45@gmail.com>
2019-03-27 13:24:17 -0400

    Cleaned up code
    Got rid of debugging code

--- script.js
+++ script.js
@@ -72,11 +72,9 @@ shuffle(teamPics);
 
 function flip(cardContainer) {
 	if (in_progress == true) {
-		console.log('no!!');
 		return false;
 	}
 	if (cardContainer.classList[1] !== 'faceup' && in_progress == false) {
-		console.log('FLIP!')
 		cardContainer.style.transform = "rotatey(180deg)";
 	    cardContainer.style.transitionDuration = "0.5s";
 	    cardContainer.classList.toggle("faceup");
@@ -99,20 +97,15 @@ function letsgo(event) {  //Passed event as an argument because Firefox does not
 	if (in_progress == true) {
 		return false;
 	}
-	console.log(event.target);
 	let card = event.target.parentNode.nextElementSibling.lastElementChild; // making card variable the image file of the team rather than the back generic image
 	let cardContainer = card.parentNode.parentNode;
 	if (checkIfFirstOrSecondGuess() === true  && in_progress == false) {
 		flip(cardContainer); // flip it's container
-		console.log('liv')
 		lastGuess = card;
 	} else {
 		let currentGuess = card;
 		flip(cardContainer);
-		console.log('bayern');
-		console.log(in_progress);
 		in_progress = true;
-		console.log(in_progress);
 		turns ++;
 		shots.innerHTML = `SHOTS: ${turns}`
 		if (checkForMatch(currentGuess) === false) {
@@ -130,7 +123,6 @@ function letsgo(event) {  //Passed event as an argument because Firefox does not
 
 function checkForMatch(currentGuess) {
 	if (currentGuess.getAttribute("alt").slice(0,-1) == lastGuess.getAttribute("alt").slice(0,-1) && currentGuess.getAttribute("alt").slice(-1) !== lastGuess.getAttribute("alt").slice(-1)) {
-		console.log("these match");
 		in_progress = false;
 		matchedCards.push(currentGuess, lastGuess);
 	} else {
@@ -143,23 +135,3 @@ function checkForWin() {
 		alert(`You win! It took you ${turns} shots on goal!`);
 	}
 }
-
-// // Say wait for flipback to finish before allowing anything else;
-
-// stuff about promises to help with the flipback:
-
-// https://codeburst.io/javascript-what-the-heck-is-a-callback-aba4da2deced
-// https://www.twilio.com/blog/2015/10/asyncawait-the-hero-javascript-deserved.html
-
-// Where i got the transform from:
-// https://stackoverflow.com/questions/28492791/pure-javascript-for-image-or-div-flip
-// I think there is something in UDEMY about using Javascript to directly mess with HTML style tag
-
-
-// CSS for the cards:
-
-// https://www.w3schools.com/howto/howto_css_flip_card.asp
-
-// https://blog.sessionstack.com/how-javascript-works-event-loop-and-the-rise-of-async-programming-5-ways-to-better-coding-with-2f077c4438b5
-
-// Check if with Belnako's thing if you can see the answers by reading the HTML
